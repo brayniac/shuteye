978335516f7bc193274022cbdc07c7fafb8109f2
Brian Martin <brayniac@gmail.com>
2015-10-25 10:37:39 -0700

    version 0.0.4
    - timespec is now Timespec
    - new sleep() function replaces direct call to per-os clock_nanosleep()
    - better cfg(target)
    - more doctest

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,10 @@
 //! ```
 //! use shuteye::*;
 //!
-//! let ts = shuteye::timespec::from_nano(1000).unwrap();
+//! let ts = Timespec::from_nano(1000).unwrap();
 //! assert_eq!(ts.get_sec(), 0);
 //! assert_eq!(ts.get_nsec(), 1000);
-//! shuteye::clock_nanosleep(1, 0, &ts, None);
+//! shuteye::sleep(ts);
 
 #![crate_type = "lib"]
 
@@ -29,57 +29,100 @@
 
 use std::fmt;
 
+const RELTIME: i32 = 0;
 const NSEC_PER_SEC: i64 = 1_000_000_000;
+const CLOCK_MONOTONIC: i32 = 1;
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq)]
-pub struct timespec {
+pub struct Timespec {
     pub tv_sec: i64,
     pub tv_nsec: i64,
 }
 
-impl timespec {
-    /// create a timespec from nanoseconds
+impl Timespec {
+
+    /// create a Timespec from nanoseconds
     ///
     /// # Example
     /// ```
     /// use shuteye::*;
     ///
-    /// assert!(shuteye::timespec::from_nano(1000).is_ok());
-    pub fn from_nano(nsec: i64) -> Result<timespec, &'static str> {
+    /// assert!(Timespec::from_nano(1000).is_ok());
+    pub fn from_nano(nsec: i64) -> Result<Timespec, &'static str> {
         if nsec < 0 {
             return Err("not implemented");
         }
-        if nsec > NSEC_PER_SEC {
+        if nsec >= NSEC_PER_SEC {
             let sec = (nsec as f64 / NSEC_PER_SEC as f64).floor() as i64;
             let nsec = nsec - (sec * NSEC_PER_SEC);
-            return Ok(timespec { tv_sec: sec, tv_nsec: nsec });
+            return Ok(Timespec { tv_sec: sec, tv_nsec: nsec });
         }
-        return Ok(timespec { tv_sec: 0, tv_nsec: nsec });
+        return Ok(Timespec { tv_sec: 0, tv_nsec: nsec });
     }
 
+    /// return seconds component of Timespec
+    ///
+    /// # Example
+    /// ```
+    /// use shuteye::*;
+    ///
+    /// let ts = Timespec::from_nano(1000000000).unwrap();
+    ///
+    /// assert_eq!(ts.get_sec(), 1);
     pub fn get_sec(self) -> i64 {
         self.tv_sec
     }
 
+    /// return nanoseconds component of Timespec
+    ///
+    /// # Example
+    /// ```
+    /// use shuteye::*;
+    ///
+    /// let ts = Timespec::from_nano(1).unwrap();
+    ///
+    /// assert_eq!(ts.get_nsec(), 1);
     pub fn get_nsec(self) -> i64 {
         self.tv_nsec
     }
+
+    /// return Timespec as nanoseconds
+    ///
+    /// # Example
+    /// ```
+    /// use shuteye::*;
+    ///
+    /// let ts = Timespec::from_nano(1000000001).unwrap();
+    ///
+    /// assert_eq!(ts.as_nsec(), 1000000001);
+    pub fn as_nsec(self) -> i64 {
+        self.tv_sec * NSEC_PER_SEC + self.tv_nsec
+    }
 }
 
-impl fmt::Debug for timespec {
+impl fmt::Debug for Timespec {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "timespec(sec: {}, nsec: {})", self.tv_sec, self.tv_nsec)
     }
 }
 
-#[cfg(all(not(target_os = "macos"), not(target_os = "ios")))]
-pub fn clock_nanosleep(id: i32, flags: i32, req: &timespec, remain: Option<&mut timespec>) -> i32 {
+pub fn sleep(ts: Timespec) -> Option<Timespec> {
+    let mut remain = Timespec::from_nano(0_i64).unwrap();
+    clock_nanosleep(CLOCK_MONOTONIC, RELTIME, &ts, Some(&mut remain));
+    if remain.get_nsec() == 0 && remain.get_sec() == 0 {
+        return None;
+    }
+    Some(remain)
+}
+
+#[cfg(target_os = "linux")]
+fn clock_nanosleep(id: i32, flags: i32, req: &Timespec, remain: Option<&mut Timespec>) -> i32 {
     extern {
         fn clock_nanosleep(clock_id: i32,
                            flags: i32,
-                           req: *const timespec,
-                           rem: *mut timespec)
+                           req: *const Timespec,
+                           rem: *mut Timespec)
                            -> i32;
     }
     match remain {
@@ -88,10 +131,10 @@ pub fn clock_nanosleep(id: i32, flags: i32, req: &timespec, remain: Option<&mut 
     }
 }
 
-#[cfg(all(target_os = "macos"))]
-pub fn clock_nanosleep(_: i32, _: i32, req: &timespec, remain: Option<&mut timespec>) -> i32 {
+#[cfg(target_os = "macos")]
+fn clock_nanosleep(_: i32, _: i32, req: &Timespec, remain: Option<&mut Timespec>) -> i32 {
     extern {
-        fn nanosleep(req: *const timespec, rem: *mut timespec) -> i32;
+        fn nanosleep(req: *const Timespec, rem: *mut Timespec) -> i32;
     }
     match remain {
         Some(p) => unsafe { nanosleep(req as *const _, p as *mut _) },
