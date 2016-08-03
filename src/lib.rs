c75100b8bbd5cdcc9ee7dbff81868bc2f6265d03
Brian Martin <brayniac@gmail.com>
2016-08-03 16:56:51 +0000

    shuteye 0.2.0
    - now takes std::time::Duration
    - Timespec is now internal only
    - cleanup cargo packaging

--- src/lib.rs
+++ src/lib.rs
@@ -9,139 +9,76 @@
 //!
 //! # Usage
 //!
-//! Include the module, create a timespec, call clock_nanosleep
+//! Include the module and call ```shuteye::sleep(Duration::new(seconds, nanoseconds))```
 //!
 //! # Example
 //!
 //! Sleep for 1 uS
 //!
 //! ```
-//! use shuteye::*;
-//!
-//! // create a timespec for the interval to sleep
-//! let ts = Timespec::from_nano(1000).unwrap();
-//! assert_eq!(ts.get_sec(), 0);
-//! assert_eq!(ts.get_nsec(), 1000);
+//! use shuteye::sleep;
+//! use std::time::Duration;
 //!
 //! // call sleep
-//! shuteye::sleep(ts);
+//! sleep(Duration::new(1, 0));
 
 #![crate_type = "lib"]
 
 #![crate_name = "shuteye"]
 
-use std::fmt;
+use std::time::Duration;
 
 const TIMER_RELTIME: i32 = 0;
-const NSEC_PER_SEC: i64 = 1_000_000_000;
 const CLOCK_MONOTONIC: i32 = 1;
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq)]
-pub struct Timespec {
-    pub tv_sec: i64,
-    pub tv_nsec: i64,
-}
-
-impl Timespec {
-    /// create a Timespec from nanoseconds
-    ///
-    /// # Example
-    /// ```
-    /// use shuteye::*;
-    ///
-    /// assert!(Timespec::from_nano(1000).is_ok());
-    pub fn from_nano(nsec: i64) -> Result<Timespec, &'static str> {
-        if nsec < 0 {
-            return Err("not implemented");
-        }
-        if nsec >= NSEC_PER_SEC {
-            let sec = (nsec as f64 / NSEC_PER_SEC as f64).floor() as i64;
-            let nsec = nsec - (sec * NSEC_PER_SEC);
-            return Ok(Timespec {
-                tv_sec: sec,
-                tv_nsec: nsec,
-            });
-        }
-        Ok(Timespec {
-            tv_sec: 0,
-            tv_nsec: nsec,
-        })
-    }
-
-    /// return seconds component of Timespec
-    ///
-    /// # Example
-    /// ```
-    /// use shuteye::*;
-    ///
-    /// let ts = Timespec::from_nano(1000000000).unwrap();
-    ///
-    /// assert_eq!(ts.get_sec(), 1);
-    pub fn get_sec(self) -> i64 {
-        self.tv_sec
-    }
-
-    /// return nanoseconds component of Timespec
-    ///
-    /// # Example
-    /// ```
-    /// use shuteye::*;
-    ///
-    /// let ts = Timespec::from_nano(1).unwrap();
-    ///
-    /// assert_eq!(ts.get_nsec(), 1);
-    pub fn get_nsec(self) -> i64 {
-        self.tv_nsec
-    }
-
-    /// return Timespec as nanoseconds
-    ///
-    /// # Example
-    /// ```
-    /// use shuteye::*;
-    ///
-    /// let ts = Timespec::from_nano(1000000001).unwrap();
-    ///
-    /// assert_eq!(ts.as_nsec(), 1000000001);
-    pub fn as_nsec(self) -> i64 {
-        self.tv_sec * NSEC_PER_SEC + self.tv_nsec
-    }
-}
-
-impl fmt::Debug for Timespec {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "timespec(sec: {}, nsec: {})", self.tv_sec, self.tv_nsec)
-    }
+struct Timespec {
+    tv_sec: i64,
+    tv_nsec: i64,
 }
 
 /// sleep for a relative time
 ///
 /// # Example
 /// ```
-/// use shuteye::*;
+/// use std::time::Duration;
+/// use shuteye::sleep;
 ///
-/// let ts = Timespec::from_nano(1000).unwrap();
-///
-/// // simple sleep
-/// shuteye::sleep(ts);
+/// // simple sleep for 1s duration
+/// sleep(Duration::new(1, 0));
 ///
 /// // remain captures remaining time from `Timespec`
-/// match shuteye::sleep(ts) {
+/// match sleep(Duration::new(1, 0)) {
 ///     Some(remain) => {
-///         // some sleep time remains
+///         // woke early - some sleep time remains
 ///     }
 ///     None => {
-///         // no sleep time remains
+///         // woke on-time or late - no sleep time remains
 ///     }
 /// }
-pub fn sleep(ts: Timespec) -> Option<Timespec> {
-    let mut remain = Timespec::from_nano(0_i64).unwrap();
+pub fn sleep(duration: Duration) -> Option<Duration> {
+    let ts = duration_to_timespec(duration);
+    let mut remain = Timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
     clock_nanosleep(CLOCK_MONOTONIC, TIMER_RELTIME, &ts, Some(&mut remain));
-    if remain.get_nsec() == 0 && remain.get_sec() == 0 {
+    if remain.tv_nsec == 0 && remain.tv_sec == 0 {
         return None;
     }
-    Some(remain)
+    Some(timespec_to_duration(remain))
+}
+
+fn duration_to_timespec(duration: Duration) -> Timespec {
+    Timespec {
+        tv_sec: duration.as_secs() as i64,
+        tv_nsec: duration.subsec_nanos() as i64,
+    }
+}
+
+fn timespec_to_duration(timespec: Timespec) -> Duration {
+    Duration::new(timespec.tv_sec as u64, timespec.tv_nsec as u32)
 }
 
 #[cfg(target_os = "linux")]
